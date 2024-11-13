4e11f2766c4c8ccc2818eff32daa26d776d36625
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-11-13 14:45:55 +0800

    Added / path

--- qrupload/api/index.js
+++ qrupload/api/index.js
@@ -37,6 +37,10 @@ app.post('/upload', upload.single('image'), async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
